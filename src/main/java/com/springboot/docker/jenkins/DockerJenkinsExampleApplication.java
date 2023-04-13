aa0c0d6640dee9e20ca4808d44b8131a23394241
payelsky <68430680+payelsky@users.noreply.github.com>
2023-04-13 15:56:23 +0530

    first commit

---
+++ src/main/java/com/springboot/docker/jenkins/DockerJenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/docker/jenkins/DockerJenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
